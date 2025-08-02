1e82232aa56678c72013575aa5a706314eb44629
galangnovi <galangnovi21@gmail.com>
2025-08-03 05:49:48 +0700

    updated 23

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -49,7 +49,7 @@ export default function Register() {
             <h1 className=" !text-5xl font-bold text-green-500 mb-4 text-center">circle</h1>
             <h2 className=" !text-2xl text-white text-center mb-3 font-semibold">Create Your account</h2>
         </div>
-        <form onSubmit={handleRegister} className="space-y-4">
+        <form onSubmit={handleRegister} className="space-y-4 p-6">
           <div>
             <input
               type="text"
